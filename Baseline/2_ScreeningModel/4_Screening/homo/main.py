3a6c98088abf9473c7e0434ed7a371bbe4a163d3
SJang <seohyeon0906@gmail.com>
2023-07-27 14:23:01 +0900

    modified main.py, 최종 반환 결과물 확인할 수 있는 코드

--- Baseline/2_ScreeningModel/4_Screening/homo/main.py
+++ Baseline/2_ScreeningModel/4_Screening/homo/main.py
@@ -11,6 +11,8 @@ from tqdm import tqdm
 # from ogb.graphproppred import Evaluator, collate_dgl
 
 import csv
+import pandas as pd
+import pickle
 
 import sys
 sys.path.append('..')
@@ -20,65 +22,34 @@ from utils.config import process_config, get_args
 from utils.lr import MultiStepLRWarmUp
 from data_preparation import MoleculeDataset
 
-import wandb
 
 torch.set_num_threads(1)
-
-def train(model, device, loader, optimizer):
-    model.train()
-    loss_all = 0
-
-    for step, (bg, labels) in enumerate(tqdm(loader, desc="Train iteration")):
-        bg = bg.to(device)
-        x = bg.ndata.pop('feat')
-        edge_attr = bg.edata.pop('feat')
-        bases = bg.edata.pop('bases')
-        labels = labels.to(device)
-
-        pred = model(bg, x, edge_attr, bases)
-        optimizer.zero_grad()
-
-        loss = F.l1_loss(pred, labels)
-        loss.backward()
-        optimizer.step()
-        loss_all = loss_all + loss.detach().item()
-    return loss_all / len(loader)
 
 
 def eval(model, device, loader):
     model.eval()
-    total_mae = 0
     predicted_labels = [] # 추가
-    real_labels = [] # 추가
 
     with torch.no_grad():
-        for step, (bg, labels) in enumerate(tqdm(loader, desc="Eval iteration")):
+        for step, bg in enumerate(tqdm(loader, desc="Eval iteration")):
             bg = bg.to(device)
             x = bg.ndata.pop('feat')
             edge_attr = bg.edata.pop('feat')
             bases = bg.edata.pop('bases')
-            labels = labels.to(device)
+        #    labels = labels.to(device)
 
             pred = model(bg, x, edge_attr, bases)
-            mae = F.l1_loss(pred, labels).detach().item()
-
-            total_mae += mae
             predicted_labels.extend(pred) # 추가
-            real_labels.extend(labels) # 추가
 
-        avg_mae = total_mae / (step + 1)
-    return avg_mae, predicted_labels, real_labels # 추가
+    return predicted_labels
 
-def save_labels_to_csv(pred_labels, real_labels, best_epoch, filepath): # 추가
+def save_labels_to_csv(pred_labels, filepath): # 추가
     with open(filepath, 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(['Index', 'Predicted Label', 'Real Label'])
+        csv_writer.writerow(['Index', 'Predicted Label'])
         for i in range(len(pred_labels)):
             pred_value = pred_labels[i].item()  # Extract the numerical value from the tensor
-            real_value = real_labels[i].item()
-            csv_writer.writerow([i, pred_value, real_value])
-
-        csv_writer.writerow(['Best Epoch', best_epoch])
+            csv_writer.writerow([i, pred_value])
 
 
 import time
@@ -89,41 +60,14 @@ def main():
     print(torch.cuda.get_device_name(0), cuda_id)
     print(config)
 
-    wandb.init(project="pdf_test",
-               config={'commit_id': config.commit_id[0:7],
-                       'cuda_id': cuda_id,
-                       'shared_filter': config.get('shared_filter', ''),
-                       'linear_filter': config.get('linear_filter', ''),
-                       'basis': config.basis,
-                       'edgehop': config.get('edgehop', ''),
-                       'epsilon': config.epsilon,
-                       'power': config.power,
-                       'degs': config.get('degs', ''),
-                       'num_layers': config.architecture.layers,
-                       'hidden_units': config.architecture.hidden,
-                       'learning_rate': config.hyperparams.learning_rate,
-                       'warmup_epochs': config.hyperparams.warmup_epochs,
-                       'milestones': config.hyperparams.milestones,
-                       'decay_rate': config.hyperparams.decay_rate,
-                       'weight_decay': config.hyperparams.weight_decay,
-                       'batch_size': config.hyperparams.batch_size,
-                       'num_workers': config.get('num_workers', 'na')},
-               save_code=True,
-               name='homo_351epochs_2')
-
-
     for seed in config.seeds:
         config.seed = seed
         config.time_stamp = int(time.time())
         print(config)
 
-        epoch_idx, valid_curve, test_curve, trainL_curve = run_with_given_seed(config)
-
-
-        best_valid_epoch = np.argmin(np.array(valid_curve))
-        print('Finished test_mae: {}, Validation_mae: {}, best_epoch: {}, best loss: {}'
-              .format(test_curve[best_val_epoch], valid_curve[best_val_epoch],
-                      best_valid_epoch,  min(trainL_curve)))
+        predicted_labels = run_with_given_seed(config) ######## return 수정해야 함.
+        
+        print('Finished label prediction')
 
 
 def run_with_given_seed(config):
@@ -141,22 +85,21 @@ def run_with_given_seed(config):
 
     print("Bases total: {}".format(dataset.train.graph_lists[0].edata['bases'].shape[1]))
 
-    trainset, valset, testset = dataset.train, dataset.val, dataset.test
+    testset = dataset.test
 
-    train_loader = DataLoader(trainset, batch_size=config.hyperparams.batch_size, shuffle=True,
-                              num_workers=config.num_workers, collate_fn=dataset.collate)
-    valid_loader = DataLoader(valset, batch_size=config.hyperparams.batch_size, shuffle=False,
-                              num_workers=config.num_workers, collate_fn=dataset.collate)
     test_loader = DataLoader(testset, batch_size=config.hyperparams.batch_size, shuffle=False,
                              num_workers=config.num_workers, collate_fn=dataset.collate)
 
+    # 인덱스 생성
+    index = [i for i in range(len(dataset.test))]
+    
     if config.dataset_name == 'QM':
-        atom_dim = 11 # atom type 개수
+        atom_dim = 5 # atom type 개수
         bond_dim = 4   # bond type 개수
     else:
         raise ValueError('Unknown dataset name {}'.format(config.dataset_name))
     model = Net(config.architecture, num_tasks=1,
-                num_basis=dataset.train.graph_lists[0].edata['bases'].shape[1],
+                num_basis=dataset.test.graph_lists[0].edata['bases'].shape[1],
                 shared_filter=config.get('shared_filter', '') == 'shd',
                 linear_filter=config.get('linear_filter', '') == 'lin',
                 atom_dim=atom_dim,
@@ -172,112 +115,58 @@ def run_with_given_seed(config):
                                   num_warm_up=config.hyperparams.warmup_epochs,
                                   init_lr=config.hyperparams.learning_rate)
 
-    epoch_idx = []
-    valid_curve = []
-    test_curve = []
-    trainL_curve = []
-
-    cur_epoch = 0
-    # if config.get('resume_train') is not None:
-    #     print("Loading model from {}...".format(config.resume_train), end=' ')
-    #     checkpoint = torch.load(config.resume_train)
-    #     model.load_state_dict(checkpoint['model_state_dict'])
-    #     model.to(device)
-    #     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    #     scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
-    #     cur_epoch = checkpoint['epoch']
-    #     cur_loss = checkpoint['loss']
-    #     lr = checkpoint['lr']
-    #     print("Model loaded.")
-    #
-    #     print("Epoch {} evaluating...".format(cur_epoch))
-    #     train_perf = eval(model, device, train_loader)
-    #     valid_perf = eval(model, device, valid_loader)
-    #     test_perf = eval(model, device, test_loader)
-    #
-    #     print('Train:', train_perf,
-    #           'Validation:', valid_perf,
-    #           'Test:', test_perf,
-    #           'Train loss:', cur_loss,
-    #           'lr:', lr)
-    #
-    #     epoch_idx.append(cur_epoch)
-    #     train_curve.append(train_perf)
-    #     valid_curve.append(valid_perf)
-    #     test_curve.append(test_perf)
-    #     trainL_curve.append(cur_loss)
-    #
-    #     writer.add_scalars('traP', {ts_fk_algo_hp: train_perf}, cur_epoch)
-    #     writer.add_scalars('valP', {ts_fk_algo_hp: valid_perf}, cur_epoch)
-    #     writer.add_scalars('tstP', {ts_fk_algo_hp: test_perf}, cur_epoch)
-    #     writer.add_scalars('traL', {ts_fk_algo_hp: cur_loss}, cur_epoch)
-    #     writer.add_scalars('lr',   {ts_fk_algo_hp: lr}, cur_epoch)
-
-    best_val = float('inf') # 추가
-    best_val_pred_labels = None # 추가
-    best_val_epoch = None # 추가
-
-    pred_labels_at_best_epoch = None # 추가
-    best_test_epoch = None
-
-    for epoch in range(cur_epoch + 1, config.hyperparams.epochs + 1):
-        lr = scheduler.optimizer.param_groups[0]['lr']
-        print("Epoch {} training...".format(epoch))
-        train_loss = train(model, device, train_loader, optimizer)
-        scheduler.step()
-
-        print('Evaluating...')
-        valid_perf, valid_labels, valid_real_labels = eval(model, device, valid_loader) # 추가
-        test_perf, test_labels, test_real_labels = eval(model, device, test_loader)  # 추가
-
-
-        print('Epoch:', epoch,
-              'Validation_mae:', valid_perf,
-              'Test_mae:', test_perf,
-              'Train loss:', train_loss,
-              'lr:', lr)
-
-        wandb.log({"epoch": epoch, "Train loss_mae": train_loss, "Val mae":valid_perf, "Test mae": test_perf, "learning rate": lr},
-                  commit=True)
-
-        epoch_idx.append(epoch)
-        valid_curve.append(valid_perf)
-        test_curve.append(test_perf)
-        trainL_curve.append(train_loss)
-
-        if config.get('checkpoint_dir') is not None:
-            filename_header = str(config.commit_id[0:7]) + '_' \
-                       + str(config.time_stamp) + '_' \
-                       + str(config.dataset_name)
-            if valid_perf < best_val: # valid_perf = accuracy(mae) of validation set
-                best_val = valid_perf
-                best_val_pred_labels = valid_labels  # mae가 가장 낮은 epoch에서 예측된 라벨을 저장
-                best_val_epoch = epoch
-
-                pred_labels_at_best_epoch = test_labels
-                best_test_epoch=epoch
-                filename = filename_header + 'best.tar'
-            else:
-                filename = filename_header + 'curr.tar'
-
-            print("Saving model as {}...".format(filename), end=' ')
-            torch.save({'epoch': epoch,
-                        'model_state_dict': model.state_dict(),
-                        'optimizer_state_dict': optimizer.state_dict(),
-                        'scheduler_state_dict': scheduler.state_dict(),
-                        'loss': train_loss,
-                        'lr': lr},
-                       os.path.join(config.checkpoint_dir, filename))
-            print("Model saved.")
-
+    
+    if config.get('resume_train') is not None:
+        print("Loading model from {}...".format(config.resume_train), end=' ')
+        checkpoint = torch.load(config.resume_train)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        model.to(device)
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        cur_epoch = checkpoint['epoch']
+        #cur_loss = checkpoint['loss']
+        lr = checkpoint['lr']
+        print("Model loaded.")
+    
+        print("Epoch {} evaluating...".format(cur_epoch))
+        predicted_labels = eval(model, device, test_loader)
+    
     # Save predicted labels and real labels in CSV files
-    save_labels_to_csv(best_val_pred_labels, valid_real_labels, best_val_epoch, config.directory + 'valid_labels.csv')  # 추가
-    save_labels_to_csv(pred_labels_at_best_epoch, test_real_labels, best_test_epoch, config.directory + 'test_labels.csv')  # 추가
+    save_labels_to_csv(predicted_labels, config.directory + 'Predicted_labels3.csv')  # 추가
+
+# 최종 반환되어야 하는 것
+#   1. Predictedlabels
+#   2. EC HOMO 보다 높은 분자 필터링, 필터링 된 분자 개수
+#   3. 필터링 된 분자의 SMILES 혹은 dgl 그래프
+    predicted_labels_tensor = torch.tensor(predicted_labels)
+    data = {"Index": index, 'homo': predicted_labels_tensor.cpu().numpy()}  ## index 리스트 data_preparation.py에서 뽑아와야 함.
+    pred_df = pd.DataFrame(data)
+    filtered_df = pred_df[pred_df['homo'] > -0.2935]
+    extracted_indices = filtered_df['Index'].tolist()
+
+    num_extracted = len(filtered_df)
+    print('Number of extracted rows: ', num_extracted)
+
+    # extracted_indices 이용해서 데이터 추출
+    extracted_data = dataset.test.get_data_by_indices(extracted_indices)
 
 
-    return epoch_idx, valid_curve, test_curve, trainL_curve
+    # 모든 returns 저장
+    # Dataframe은 CSV로 저장
+    pred_df.to_csv(os.path.join(config.directory, 'pred_df.csv'), index=False)
+    filtered_df.to_csv(os.path.join(config.directory, 'filtered_df.csv'), index=False)
+
+    # extracted indices 리스트는 text file로 저장
+    with open(os.path.join(config.directory, 'extracted_indices.txt'), 'w') as f:
+        for idx in extracted_indices:
+            f.write(str(idx) + '\n')
+
+    # extracted_data는 pickle file로 저장
+    with open(os.path.join(config.directory, 'extracted_data.pkl'), 'wb') as f:
+        pickle.dump(extracted_data, f)
+
+    return predicted_labels , filtered_df, extracted_indices, extracted_data
 
 if __name__ == "__main__":
     main()
 
-wandb.finish()
